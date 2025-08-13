9fa662dae05b59d10ba4c78ced751c20e9823ea5
Calum H. (IMB11) <hendersoncal117@gmail.com>
2025-08-13 12:13:33 +0100

    feat: Initial commit - set up structure

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import eslint from '@eslint/js'
+import prettierEslint from 'eslint-plugin-prettier/recommended'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierEslint,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-type-alias': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/prefer-literal-enum-member': 'off',
+    },
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'build/'],
+  },
+)
