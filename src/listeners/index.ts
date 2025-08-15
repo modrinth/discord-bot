99ea9cd3ff486b2770b3e61354b42861d62a1b21
Nitrrine <nitrrine@team.lodine.xyz>
2025-08-15 16:53:52 +0500

    feat: add check if using forum for modrinth products

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -2,12 +2,14 @@ import { MessageListener } from '../types'
 import { greetCommunitySupport } from './forum/greet'
 import { lockOnOpDeletesStarter } from './forum/lock-on-op-delete-starter'
 import { remindSolvedCreate, remindSolvedUpdate } from './forum/solved-reminder'
+import {checkIfModrinthProduct} from "@/listeners/forum/check-if-modrinth-product";
 
 const listeners: MessageListener[] = [
     greetCommunitySupport,
     remindSolvedCreate,
     remindSolvedUpdate,
     lockOnOpDeletesStarter,
+    checkIfModrinthProduct,
 ]
 
 export default listeners
