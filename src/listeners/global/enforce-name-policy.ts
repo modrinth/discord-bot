03abf31c2489db9aa405fe0cc57fc824bb56337e
Nitrrine <nitrrine@team.lodine.xyz>
2025-08-21 10:58:48 +0500

    feat: enforce name policy

---
+++ src/listeners/global/enforce-name-policy.ts
@@ -0,0 +1,38 @@
+import { CreateListener } from '@/types'
+import { isByActivatedUser } from '@/utils/users'
+import { GuildMember } from 'discord.js'
+
+export const enforceNamePolicy: CreateListener = {
+	id: 'global:enforce-name-policy',
+	event: 'create',
+	description: 'Enforces name policy',
+	priority: 0,
+	filter: { allowBots: false, allowDMs: false },
+	match: async () => true,
+	handle: async (ctx) => {
+		if (!ctx.message.guild) return
+		if (!ctx.message.author) return
+
+		const member = ctx.message.guild.members.cache.get(ctx.message.author.id) as GuildMember
+		if (!member) return
+
+		// Skip enforcement for activated members
+		if (isByActivatedUser(member)) return
+
+		const displayName = member.displayName.toLowerCase()
+
+		const namePolicy = /^[\u0020-\u024F\-'. ]+$/u
+
+		if (!namePolicy.test(displayName)) {
+			// At this point, name violates the policy.
+			console.log(`Name policy violation: ${displayName} (${member.id})`)
+			try {
+				await member.setNickname(member.user.username.toUpperCase(), 'Name policy violation')
+			} catch (err) {
+				console.error(`Failed to update nickname for ${member.id}:`, err)
+			}
+
+			// 	TODO: Log this action
+		}
+	},
+}
--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -6,6 +6,7 @@ import { lockOnOpDeletesStarter } from './forum/lock-on-op-delete-starter'
 import { remindSolvedCreate, remindSolvedUpdate } from './forum/solved-reminder'
 import { countMessages } from '@/listeners/global/count-messages'
 import { scanForBlocklistedFiles } from '@/listeners/global/scan-for-blocklisted-files'
+import { enforceNamePolicy } from '@/listeners/global/enforce-name-policy'
 
 const listeners: MessageListener[] = [
 	greetCommunitySupport,
@@ -15,6 +16,7 @@ const listeners: MessageListener[] = [
 	checkIfModrinthProduct,
 	countMessages,
 	scanForBlocklistedFiles,
+	enforceNamePolicy,
 ]
 
 export default listeners
