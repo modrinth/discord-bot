d5bf0809def1e1932e665efdb2353d3a93b3f609
Nitrrine <nitrrine@team.lodine.xyz>
2025-09-18 22:05:33 +0500

    refactor: remove unused import

--- src/data/forum/solved-reminder.ts
+++ src/data/forum/solved-reminder.ts
@@ -1,3 +1,1 @@
-import * as process from 'node:process'
-
 export default `-# <:cornerdownright:${process.env.CORNER_DOWN_RIGHT_EMOJI_ID}> Command suggestion: </solved:${process.env.SOLVED_COMMAND_ID}>`
