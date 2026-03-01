d5c8df1c48766f86827ef53b57a4b03ef740cb95
Nitrrine <nitrrine@team.lodine.xyz>
2026-03-01 12:23:49 +0500

    feat: active role removed embed

--- src/data/dms/active-role-granted.embed.ts
+++ src/data/dms/active-role-granted.embed.ts
@@ -1,6 +1,7 @@
 import { EmbedData } from 'discord.js'
 
 const data: EmbedData = {
+	title: "We've automatically granted you an active role",
 	description: [
 		'You are now permitted to upload images and share links in our community.',
 		'Please use this privilege responsibly, abuse will result in removal of this role.',
---
+++ src/data/dms/active-role-removed.embed.ts
@@ -0,0 +1,13 @@
+import { EmbedData } from 'discord.js'
+
+const data: EmbedData = {
+	title: 'Removal of active role',
+	description: [
+		"In regards of your recent behaviour or individual moderator discretion, we've removed your active role.",
+		'Right now your discord account is indefinitely suspended from becoming activated back.',
+		'Please contact us via ModMail if you believe this was an error.',
+	].join('\n'),
+	color: 0xff496e,
+}
+
+export default data
